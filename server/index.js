7d9292ab12863963c0cffe749d0c64c4c0ced727
IVMetodiev23 <IVMetodiev23@codingburgas.bg>
2025-05-20 23:10:03 +0300

    Add Express server and basic route

---
+++ server/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+
+const App = express()
+
+App.use(express.json())
+
+App.get('/', (req, res) => {
+    res.status(200).json({message: "All working"})
+})
+
+const start = async () => {
+    App.listen(process.env.PORT, () => console.log(`Server started at port ${process.env.PORT}`))
+}
+
+start()
