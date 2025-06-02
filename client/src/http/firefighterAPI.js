63d0a5e8f3d3f506608abe06f0be2df132bdfa4d
IVMetodiev23 <IVMetodiev23@codingburgas.bg>
2025-06-02 18:42:40 +0300

    Add firefighter API

---
+++ client/src/http/firefighterAPI.js
@@ -0,0 +1,28 @@
+import { $authHost, $host } from './index'
+
+export const getFirefighters = async (limit = 5, page) => {
+    const {data} = await $host.get('/api/firefighter', {params: {
+        limit, page
+    }})
+    return data
+}
+
+export const getOneFirefighter = async (id) => {
+    const {data} = await $host.get(`/api/firefighter${id}`)
+    return data
+}
+
+export const addFirefighter = async (firefighter) => {
+    const {data} = await $host.get('/api/firefighter', firefighter)
+    return data
+}
+
+export const deleteFirefighter = async (id) => {
+    const {data} = await $host.get('/api/firefighter', id)
+    return data
+}
+
+export const assignFirefighter = async (id, fireTeamId) => {
+    const {data} = await $host.get(`/api/${id}/assign-to-team`, fireTeamId)
+    return data
+}
