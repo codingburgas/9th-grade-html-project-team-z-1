142165cc492a732a7976042267f63f940f4eb7d5
Ekaterina Zalinskaya <EDZalinskaya23@codingburgas.bg>
2025-06-06 22:45:39 +0300

    Add functionality to login page

--- client/src/components/modals/login.js
+++ client/src/components/modals/login.js
@@ -1,20 +1,42 @@
 import { observer } from "mobx-react-lite";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../..";
 import style from "../styles/login.css"
+import {login} from "../../http/userAPI"
 
 export const Login = observer(()=> {
     const {user} = useContext(Context)
+    const [email, setEmail]=useState("")
+    const [password, setPassword]=useState("")
+    const click = async() => {
+        try{
+            let data
+            data = await login(email, password)
+            user.setUser(user)
+            user.setIsAuth(true)
+        }
+        catch(e) {
+            alert(e.response.data.message)
+        }
+    }
+
     return (
         <div className="loginForm">
             <div className="Primary">
                 <h2>Log in</h2>
             <form>
-                <input type="text" placeholder="Enter email"></input>
-                <input type="text" placeholder="Enter password"></input><br></br>
-                <input type="submit"></input>
+                <input type="text"
+                placeholder="Enter email" 
+                value={email}
+                onChange={e => setEmail(e.target.value)}></input>
+                
+                <input type="text" 
+                placeholder="Enter password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}></input><br></br>
+                <input type ="submit" onClick={click}></input>
             </form>
-                <h4>Don't have an account? Register</h4>
+                <h5>Don't have an account? Register</h5>
             </div>
         </div>
     )
--- client/src/http/userAPI.js
+++ client/src/http/userAPI.js
@@ -1,20 +1,20 @@
 import { $authHost, $host } from "./index";
-import jwt_decode from 'jwt-decode'
+import {jwtDecode} from 'jwt-decode'
 
 export const registration = async (firstName, lastName, email, password) => {
     const {data} = await $host.post('api/user/registration', {firstName, lastName, email, password, role: 'ADMIN'})
     localStorage.setItem('token', data.token)
-    return jwt_decode(data.token)
+    return jwtDecode(data.token)
 }
 
 export const login = async (email, password) => {
     const {data} = await $host.post('api/user/login', {email, password})
     localStorage.setItem('token', data.token)
-    return jwt_decode(data.token)
+    return jwtDecode(data.token)
 }
 
 export const check = async () => {
     const {data} = await $authHost.get('api/user/auth')
     localStorage.setItem('token', data.token)
-    return jwt_decode(data.token)
+    return jwtDecode(data.token)
 }
