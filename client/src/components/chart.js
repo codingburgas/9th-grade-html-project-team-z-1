6650df192c1ad0f1ca440ed08f2de84db674b0af
IRkeserdzhieva23 <IRkeserdzhieva23@codingburgas.bg>
2025-06-08 23:29:21 +0300

    Fix chart

--- client/src/components/chart.js
+++ client/src/components/chart.js
@@ -1,5 +1,7 @@
 import { observer } from "mobx-react-lite";
-import React from "react";
+import "./styles/chart.css" 
+import {fetchAccidents, fetchTypes} from "../http/accidentAPI";
+import React, { useContext, useEffect, useState } from "react";
 import { 
     Chart as ChartJS, 
     ArcElement, 
@@ -8,46 +10,56 @@ import {
     Title
 }from "chart.js";
 import {Pie} from "react-chartjs-2";
+import { Context } from "../index";
 ChartJS.register(ArcElement, Tooltip, Legend, Title);
-const data = {
-    labels: [
-        "Fires",
-       " Rescues",
-       " CarCrashes"
-    ], 
-    datasets: [{
-        label: "Accidents number", 
-        data:[15, 20, 30],
-        backgroundColor: [ 
-            "#ffbb00",
-            "#0877ff",
-            "#ff051e"
-        ],
-        borderColor: "#000000",
-        borderWidth: 2
-    }]
-    
-}
-const options = {
-    responsive: true,
-    plugins: {
-        legend: {
-            position: "right"
-        },
-        title:{
-            display: true,
-            "text":"title",
-            font: {
-                size: 15
+export const Chart = observer(( ) => 
+    {
+        const {accident} = useContext(Context)
+        const[data, setData] = useState()
+          useEffect(() =>{
+            let types= [];
+            fetchTypes().then(res => res.json()).then(data=> {
+                const labels = data.map(item => console.log(item.name))
+                setData( {
+               labels, 
+               datasets: [{
+                   label: "Accidents number", 
+                   data:[15, 20, 30],
+                   backgroundColor: [ 
+                       "#ffbb00",
+                       "#0877ff",
+                       "#ff051e"
+                    ],
+                    borderColor: "#000000",
+                    borderWidth: 2
+                }]
+                
+            }) 
+            } )
+           console.log(data)
+
+          
+            
+        }, [])
+        const options = {
+            responsive: false,
+            plugins: {
+                legend: {
+                    position: "right"
+                },
+                title:{
+                    display: true,
+                    "text":"title",
+                    font: {
+                        size: 15
+                    }
+                }
             }
         }
-    }
-}
-export const Chart = observer(( ) => 
-{
-    return (
-        <div className="chart">
-      <Pie className="pie" data = {data} options = {options}/>
-      </div>
+       
+        return (
+   <div className="chart">
+      <Pie  data = {data} options = {options}  width={700} height={700} />
+    </div>
     )
 })
