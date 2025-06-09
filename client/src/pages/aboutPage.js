0da13ffda4946879594e3baab5542d44fa1b9d29
IRkeserdzhieva23 <IRkeserdzhieva23@codingburgas.bg>
2025-06-10 00:00:32 +0300

    Add about us page

--- client/src/pages/aboutPage.js
+++ client/src/pages/aboutPage.js
@@ -1,9 +1,66 @@
 import React from 'react'
+import "./styles/aboutPage.css" 
 
 const aboutPage = () => {
   return (
-    <div>
-        About page
+    <div className='aboutUs'>
+    <div class="card">
+            <div class="photo photo1">
+              <div class="info-cont">
+                <h2 class="name">David Papazyan</h2>
+                <h3 class="role">Scrum Trainer</h3>
+                <p class="info text">
+                  Погрижи се сайта да изглежда модерно, изработи дизайна,
+                  създаде логото на отбора и подбра цветовата палитра.
+                </p>
+                <h2 class="plhn">David Papazyan</h2>
+              </div>
+            </div>
+          </div>
+      
+          <div class="card">
+            <div class="photo photo4">
+              <div class="info-cont">
+                <h2 class="name">Ivelin Metodiev</h2>
+                <h3 class="role">Front-end Developer</h3>
+                <p class="info text">
+                  Погрижи се сайта да изглежда модерно, изработи дизайна,
+                  създаде логото на отбора и подбра цветовата палитра.
+                </p>
+                <h2 class="plhn">Ivelin Metodiev</h2>
+              </div>
+            </div>
+          </div>
+
+<div class="card">
+            <div class="photo photo3">
+              <div class="info-cont">
+                <h2 class="name">Ekaterina Zelinskaya</h2>
+                <h3 class="role">Design</h3>
+                <p class="info text">
+                  Погрижи се сайта да изглежда модерно, изработи дизайна,
+                  създаде логото на отбора и подбра цветовата палитра.
+                </p>
+                <h2 class="plhn"> Ekaterina Zelinskaya</h2>
+              </div>
+            </div>
+          </div>
+
+<div class="card">
+            <div class="photo photo2">
+              <div class="info-cont">
+                <h2 class="name">Ivayla Keserdzhieva</h2>
+                <h3 class="role">Back-end Developer</h3>
+                <p class="info text">
+                  Погрижи се сайта да изглежда модерно, изработи дизайна,
+                  създаде логото на отбора и подбра цветовата палитра.
+                </p>
+                <h2 class="plhn">Ivayla Keserdzhieva</h2>
+              </div>
+            </div>
+          </div>
+      
+
     </div>
   );
 }
