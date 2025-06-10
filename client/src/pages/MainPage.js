c70b0267facf68ee62bd9aa9f2695d8e08b6d0d0
IRkeserdzhieva23 <IRkeserdzhieva23@codingburgas.bg>
2025-06-10 22:33:31 +0300

    Fix main page

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './styles/mainPage.css'
+import { REPORT_A_FIRE } from '../utils/consts'
+import {NavLink} from 'react-router-dom'
 const MainPage = () => {
   return (
     <div>
       <img src = "assets/ZOF_logo.PNG" className='main_logo' alt='logo'/>
       <div className='container'><h2>We are Team Z(et on Fire), and our mission is to create a website where you can report fire incidents happening in your area. Our platform is designed to make filling out the form easy for everyone. Additionally, we provide access to our team information and charts.</h2>
-    
-    </div></div>
+
+    </div>
+     <button className='button'><NavLink to={REPORT_A_FIRE}>Report a fire</NavLink></button></div>
   );
 }
 
